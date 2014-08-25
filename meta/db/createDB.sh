bece32540ecbbba5c793a65d4d88bb6d6d984cfd
hrj <harshad.rj@gmail.com>
2014-08-25 18:14:49 +0530

    fixed minor bug in createDB script
    was referencing old version of h2 jar

--- meta/db/createDB.sh
+++ meta/db/createDB.sh
@@ -2,7 +2,7 @@ set -e -x
 
 rm -rf ../../Platform_Core/info/gngr/db/
 
-H2_JAR=../../Platform_Core/lib/h2-1.4.179.jar
+H2_JAR=../../Platform_Core/lib/h2-1.4.180.jar
 
 java -cp $H2_JAR org.h2.tools.RunScript -url "jdbc:h2:./test-db.h2" -user "sa" -password "" -script ../../Platform_Core/info/gngr/schema.sql
 
